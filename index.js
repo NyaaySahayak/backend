800a922d028076eecf1819757b75492f4620fa76
Keshav Jadhav <jadavkeshav2005@gmail.com>
2023-12-15 14:52:21 +0530

    First commit of our backend server

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+dotenv.config();
+const cors = require('cors');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas');
+  app.get('/data', async (req, res) => {
+    try {
+      const queCollection = db.collection('que');
+      const data = await queCollection.find({}).toArray();
+      console.log('Fetched data:', data); 
+      res.json(data);
+    } catch (error) {
+      console.error('Error fetching data from MongoDB:', error);
+      res.status(500).json({ error: 'Error fetching data from MongoDB' });
+    }
+  });
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running at http://localhost:${process.env.PORT}`);
+  });
+});
